b857312abf690fc7caa6fe324c0ca19c88896952
Sylvie-B <unsolo@hotmail.fr>
2020-12-01 14:27:32 +0100

    en commencant par fizz buzz

--- appFizzBuzz.js
+++ appFizzBuzz.js
@@ -1,12 +1,12 @@
 for (let i = 1; i < 101 ; i++){
-    if (i % 3 === 0 && i % 5 !== 0){
-        console.log( i + "=> fizz" );
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log( i + "=> fizz buzz");
     }
-    else if (i % 3 !== 0 && i % 5 === 0){
+    else if (i % 5 === 0){
         console.log( i + "=> buzz");
     }
-    else if (i % 3 === 0 && i % 5 === 0){
-        console.log( i + "=> fizz buzz");
+    else if (i % 3 === 0){
+        console.log( i + "=> fizz" );
     }
     else {
         console.log( i + "=>" + i );
